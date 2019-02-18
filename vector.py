0ab19fe038ee6e0a9fec1d97ecbeed4455c55d46
Ricardo Martinez <remo87@gmail.com>
2019-02-18 11:19:42 -0500

    add substract scale

--- vector.py
+++ vector.py
@@ -19,3 +19,37 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def sum(self, v):
+        
+        if(self.dimension != v.dimension):
+            raise ValueError('Vectors must be of the same dimension')
+        sumVector = [x+y for x,y in zip(self.coordinates, v.coordinates)]
+        return Vector(sumVector)
+    
+    def substract(self, v):
+        
+        if(self.dimension != v.dimension):
+            raise ValueError('Vectors must be of the same dimension')
+        
+        substract = [x-y for (x,y) in zip(self.coordinates, v.coordinates)]
+        
+        substractVector = Vector (substract)
+        return substractVector
+
+    def scale(self, c):
+        scale = [c*x for x in self.coordinates]
+        scaleVector = Vector (scale)
+        return scaleVector
+
+vec1 = Vector([8.218,-9.341])
+vec2 = Vector([-1.129,2.111])
+print(vec1.sum(vec2))
+
+vec1 = Vector([7.119,8.215])
+vec2 = Vector([-8.223,0.878])
+print(vec1.substract(vec2))
+
+vec1 = Vector([1.671,-1.012,-0.318])
+scalar = 7.41
+print(vec1.scale(scalar))
